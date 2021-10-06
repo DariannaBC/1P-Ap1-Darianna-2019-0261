12be5b1a105a42174b5826854ee2b021041b6eea
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-10-05 20:36:53 -0400

    Creando las entidades

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1P_Ap1_Darianna_2019_0261.Entidades
+{
+   public  class Aportes
+    {
+        [Key]
+        public int aporteID { get; set; }
+        public DateTime fecha { get; set; }
+        public String persona { get; set; }
+        public String concepto { get; set; }
+        public float monto { get; set; }
+
+        public Aportes()
+        {
+            aporteID = 0;
+            fecha = DateTime.Now;
+            persona = string.Empty;
+            concepto = string.Empty;
+            monto = 0;
+        }
+    }
+}
