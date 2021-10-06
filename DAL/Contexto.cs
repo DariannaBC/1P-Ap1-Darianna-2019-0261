bfad4961432e506edb52a51c53f86646bfb2ecef
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-10-05 20:38:56 -0400

    Creando el Contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1P_Ap1_Darianna_2019_0261.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace _1P_Ap1_Darianna_2019_0261.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"data source = Data\aportescontrol.db");
+        }
+    }
+}
