4eebb02c194434eda8d69929c721762bb768c485
Darianna Cruz De La Rosa <darianna_cruz@ucne.edu.do>
2021-10-05 20:48:03 -0400

    Creando la BLL

---
+++ BLL/Utilidades.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1P_Ap1_Darianna_2019_0261.BLL
+{
+    public class Utilidades
+    {
+        public static int ToInt(string valor)
+        {
+            int retorno;
+
+            int.TryParse(valor, out retorno);
+
+            return retorno;
+        }
+    }
+}
